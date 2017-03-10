b339bc8e6b2676c264968cabe85c86d6df2dd320
peck liu <xiaoyang20170204@gmail.com>
2017-03-11 01:47:15 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
+
 #destory
 def destroy
   @group = Group.find(params[:id])
@@ -16,7 +17,7 @@ end
 #create
 def create
   @group = Group.new(group_params)
-
+  @group.user = current_user
   if @group.save
     redirect_to groups_path
   else
